2824e43855173a8590b3c4389d8330938560c4cd
Yaxin Cheng <yaxin.cheng@dal.ca>
2019-01-08 20:55:13 -0500

    Initial Commit

---
+++ MathParser/MathParser.h
@@ -0,0 +1,19 @@
+//
+//  MathParser.h
+//  MathParser
+//
+//  Created by Yaxin Cheng on 2019-01-08.
+//  Copyright © 2019 Yaxin Cheng. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MathParser.
+FOUNDATION_EXPORT double MathParserVersionNumber;
+
+//! Project version string for MathParser.
+FOUNDATION_EXPORT const unsigned char MathParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MathParser/PublicHeader.h>
+
+
